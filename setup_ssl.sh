cf1f4f8883ef17c14aafeaae0afa1a18dbf3f485
Kai Erik Niermann <kai.niermann@gmail.com>
2023-07-29 18:04:39 +0200

    deployment fix

---
+++ setup_ssl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+email_arg="--email k.e.niermann@student.vu.nl"
+domain_args="asdfxyz.xyz"
+
+docker compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $email_arg \
+    $domain_args \
+    --agree-tos \
+    --force-renewal" certbot
+echo
